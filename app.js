5ab32c4d4c15e4bc7e8a22b31fd4a8888370d7bc
MiguelOviedoRojas <78774210+MiguelOviedoRojas@users.noreply.github.com>
2022-01-03 12:09:58 -0600

    Prueba JavaScript
    Se hacen pruebas de variables JS

---
+++ app.js
@@ -0,0 +1,53 @@
+//Uso de Variables
+/*var nombre = 'Miguel';
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad = 35;
+console.log(edad);
+console.log(typeof(edad));
+
+edad = 'Cinco';
+console.log(edad);
+console.log(typeof(edad));
+
+
+var sueldo = 1900.99;
+console.log(sueldo);
+console.log(typeof(sueldo));
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+console.log(typeof(tieneTrabajo));
+
+var puestoDeTrabajo;
+console.log(puestoDeTrabajo);
+
+puestoDeTrabajo = null;
+console.log(puestoDeTrabajo);*/
+
+/*
+Operadores Matematicos
+    +  -  /  *
+*/
+
+var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2022;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadAna + edadMaria;
+
+yearAna = yearActual - edadAna;
+yearMaria = yearActual - edadMaria;
+
+console.log(diferenciaEdad);
+console.log(sumaEdades);
+console.log('Año en que nació Ana: ' + yearAna);
+console.log('Año en que nació María: ' + yearMaria);
+console.log(yearActual * 5);
+console.log(yearActual / 2);
+
